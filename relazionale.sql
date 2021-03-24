f5b24b5bebfddf5243708cf37b7f39428329bc6e
Firamon <calabrigo.massimo@gmail.com>
2021-03-25 00:27:56 +0100

    added dataset to the db

--- relazionale.sql
+++ relazionale.sql
@@ -164,7 +164,7 @@ create table accettato(
     dataDiInizio date,
     cf char(16),
     tipoDiSpecializzazione varchar(20),
-    ambulatorio int not null,
+    ambulatorio char(1) not null,
     constraint accettato_pk primary key (data,dataDiInizio,cf,tipoDiSpecializzazione),
     constraint fk_appuntamento_accettato foreign key (data,dataDiInizio,cf,tipoDiSpecializzazione) references appuntamento (data,dataDiInizio,cf,tipoDiSpecializzazione) on delete cascade on update cascade);
 
@@ -187,3 +187,113 @@ create table ausiliarioAppuntamento(
     constraint ausiliarioAppuntamento_pk primary key (codicePersonale,data,DataDiInizio,cf,tipoDiSpecializzazione),
     constraint fk_appuntamento_ausiliario_appuntamento foreign key (data,dataDiInizio,cf,tipoDiSpecializzazione) references accettato (data,dataDiInizio,cf,tipoDiSpecializzazione) on delete cascade on update cascade,
     constraint fk_ausiliario_ausiliario_appuntamento foreign key (codicePersonale) references membroPersonaleAusiliario (codicePersonale) on delete cascade on update cascade);
+
+
+insert into medico values ('a1234567', 3459386039, 'via 20 settembre 1', 'rossi','gianni', 'interno', 2, null);
+insert into medico values ('b1234567', 5495849598, 'via del popolo 10', 'verdi', 'mauro', 'interno', 6, null);
+insert into medico values ('c1234567', 9032498764, 'viale Garibaldi 20', 'bianchi', 'anna', 'esterno', null, 55);
+insert into medico values ('d1234567', 9875324766, 'via flaminia 1', 'montecristo', 'michelle', 'esterno', null, 46);
+
+insert into membroPersonaleAusiliario values ('a0112358', 3459386036, 'via flaminia 10', 'bianchi', 'luca', 'assistente medico');
+insert into membroPersonaleAusiliario values ('b0112358', 3459386036, 'via del popolo 5', 'neri', 'giacomo', 'amministrativo');
+insert into membroPersonaleAusiliario values ('c0112358', 3459386036, 'viale Garibaldi 6', 'rossi', 'raimondo', 'assistente medico');
+insert into membroPersonaleAusiliario values ('d0112358', 3459386036, 'via del popolo 5', 'marroni', 'luca', 'entrambi');
+insert into membroPersonaleAusiliario values ('e0112358', 3459386036, 'viale Garibaldi 78', 'montecristo', 'giocanni', 'entrambi');
+
+insert into corsoDiAggiornamento values ('associazione medici', 'corso sulle iniezioni', '2021-03-20');
+insert into corsoDiAggiornamento values ('associazione ortopedici', '1000 modi per martellare le ossa', '2021-01-22');
+insert into corsoDiAggiornamento values ('associazione ortopedici', 'le protesi moderne', '2021-01-22');
+
+inserto into partecipa values ('a0112358', 'associazione medici', 'corso sulle iniezioni', '2021-03-20');
+inserto into partecipa values ('a0112358', 'associazione ortopedici', '1000 modi per martellare le ossa', '2021-01-22');
+inserto into partecipa values ('a0112358', 'associazione ortopedici', 'le protesi moderne', '2021-01-22');
+
+insert into qualifica values ('diploma di ragioneria');
+insert into qualifica values ('tecnico radiologo');
+insert into qualifica values ('laurea in infermieristica');
+
+insert into qualificare values ('a0112358', 'tecnico radiologo');
+insert into qualificare values ('b0112358', 'laurea in infermieristica');
+insert into qualificare values ('c0112358', 'laurea in infermieristica');
+insert into qualificare values ('d0112358', 'diploma di ragioneria');
+insert into qualificare values ('e0112358', 'diploma di ragioneria');
+
+insert into specializzazione values ('ortopedia');
+insert into specializzazione values ('oculistica');
+insert into specializzazione values ('radiologia');
+insert into specializzazione values ('urologia');
+
+insert into specializzare values ('a1234567', 'ortopedia');
+insert into specializzare values ('b1234567', 'radiologia');
+insert into specializzare values ('c1234567', 'ortopedia');
+insert into specializzare values ('d1234567', 'oculistica');
+insert into specializzare values ('a1234567', 'oculistica');
+insert into specializzare values ('b1234567', 'urologia');
+
+insert into paziente values ('a011235812213455', 'gianni', 'rossi', 'via del proletario 1', 8475948033, '1976-01-12', 'regolare', 45);
+insert into paziente values ('b011235812213455', 'carla', 'neri', 'via del proletario 2', 8475948033, '1960-01-12', 'regolare', 61);
+insert into paziente values ('c011235812213455', 'girolamo', 'rossi', 'via del proletario 3', 8475948033, '1999-01-12', 'occasionale', 22);
+insert into paziente values ('d011235812213455', 'anna', 'neri', 'via del proletario 4', 8475948033, '1980-01-12', 'occasionale', 41);
+insert into paziente values ('e011235812213455', 'luca', 'verdi', 'via del proletario 5', 8475948033, '2002-01-12', 'entrambi', 19);
+insert into paziente values ('f011235812213455', 'luisa', 'montecristo', 'via del proletario 6', 8475948033, '1928-01-12', 'entrambi', 93);
+
+insert into terapiaProlungata values ('2020-01-01','a011235812213455', '2020-06-30', 'chiusa', 'radiologia' ,5);
+insert into terapiaProlungata values ('2021-02-22','a011235812213455', null, 'aperta', 'urologia' , 2);
+insert into terapiaProlungata values ('2021-01-01','a011235812213455', null, 'aperta', 'oculistica' ,9);
+insert into terapiaProlungata values ('2021-01-01','b011235812213455', null, 'aperta', 'ortopedia' ,1);
+insert into terapiaProlungata values ('2019-01-01','e011235812213455', '2019-06-30', 'chiusa', 'urologia' ,14);
+insert into terapiaProlungata values ('2020-01-01','e011235812213455', '2020-06-30', 'chiusa', 'ortopedia' ,3);
+insert into terapiaProlungata values ('2021-01-02','f011235812213455', null, 'aperta', 'oculistica' ,1);
+insert into terapiaProlungata values ('2019-02-13','e011235812213455', '2019-07-30', 'chiusa', 'radiologia' ,6);
+
+insert into seduta values ('2021-01-02', 12, 'A', 'c011235812213455');
+insert into seduta values ('2020-03-12', 13, 'A', 'c011235812213455');
+insert into seduta values ('2020-05-06', 8, 'B', 'd011235812213455');
+insert into seduta values ('2019-09-15', 17, 'B', 'e011235812213455');
+insert into seduta values ('2018-12-02', 12, 'C', 'f011235812213455');
+
+insert into medicoSeduta values ('2021-01-02', 12, 'A', 'c011235812213455', 'a1234567');
+insert into medicoSeduta values ('2021-01-02', 12, 'A', 'c011235812213455', 'b1234567');
+insert into medicoSeduta values ('2021-01-02', 12, 'A', 'c011235812213455', 'c1234567');
+insert into medicoSeduta values ('2020-03-12', 13, 'A', 'c011235812213455', 'b1234567');
+insert into medicoSeduta values ('2020-05-06', 8, 'B', 'd011235812213455', 'b1234567');
+insert into medicoSeduta values ('2019-09-15', 17, 'B', 'e011235812213455', 'c1234567');
+insert into medicoSeduta values ('2018-12-02', 12, 'C', 'f011235812213455', 'd1234567');
+insert into medicoSeduta values ('2018-12-02', 12, 'C', 'f011235812213455', 'a1234567');
+
+insert into ausiliarioAppuntamento values ('2021-01-02', 12, 'A', 'c011235812213455', 'a0112358');
+insert into ausiliarioAppuntamento values ('2020-03-12', 13, 'A', 'c011235812213455', 'a0112358');
+insert into ausiliarioAppuntamento values ('2020-03-12', 13, 'A', 'c011235812213455', 'c0112358');
+insert into ausiliarioAppuntamento values ('2019-09-15', 17, 'B', 'e011235812213455', 'd0112358');
+insert into ausiliarioAppuntamento values ('2018-12-02', 12, 'C', 'f011235812213455', 'e0112358');
+
+insert into appuntamento values ('2021-04-20','2021-02-22','a011235812213455', 'urologia' ,10);
+insert into appuntamento values ('2021-04-27','2021-01-01','a011235812213455', 'oculistica' ,12);
+insert into appuntamento values ('2021-05-27','2021-01-01','b011235812213455', 'ortopedia' ,8);
+insert into appuntamento values ('2021-06-28','2021-01-02','f011235812213455', 'oculistica' ,18);
+
+insert into appuntamento values ('2019-01-01','2019-01-01','e011235812213455', 'urologia' ,13);
+insert into appuntamento values ('2020-01-01','2020-01-01','e011235812213455', 'ortopedia' ,12);
+insert into appuntamento values ('2019-02-13','2020-01-01','a011235812213455', 'radiologia' ,18);
+
+insert into programmato values ('2021-04-20','2021-02-22','a011235812213455', 'urologia' ,10);
+insert into programmato values ('2021-04-27','2021-01-01','a011235812213455', 'oculistica' ,12);
+insert into programmato values ('2021-05-27','2021-01-01','b011235812213455', 'ortopedia' ,8);
+insert into programmato values ('2021-06-28','2021-01-02','f011235812213455', 'oculistica' ,18);
+
+insert into accettato values ('2019-01-01','2019-01-01','e011235812213455', 'urologia' ,'A');
+insert into accettato values ('2020-01-01','2020-01-01','e011235812213455', 'ortopedia' ,'A');
+insert into accettato values ('2019-02-13','2020-01-01','a011235812213455', 'radiologia' ,'B');
+
+insert into medicoAppuntamento values ('b1234567', '2019-01-01','2019-01-01','e011235812213455', 'urologia');
+insert into medicoAppuntamento values ('a1234567', '2020-01-01','2020-01-01','e011235812213455', 'ortopedia');
+insert into medicoAppuntamento values ('c1234567', '2020-01-01','2020-01-01','e011235812213455', 'ortopedia');
+insert into medicoAppuntamento values ('b1234567', '2019-02-13','2020-01-01','a011235812213455', 'radiologia');
+
+insert into ausiliarioAppuntamento values ('a0112358', '2019-01-01','2019-01-01','e011235812213455', 'urologia');
+insert into ausiliarioAppuntamento values ('c0112358', '2020-01-01','2020-01-01','e011235812213455', 'ortopedia');
+insert into ausiliarioAppuntamento values ('c0112358', '2019-02-13','2020-01-01','a011235812213455', 'radiologia');
+insert into ausiliarioAppuntamento values ('d0112358', '2019-02-13','2020-01-01','a011235812213455', 'radiologia');
+insert into ausiliarioAppuntamento values ('e0112358', '2020-01-01','2020-01-01','e011235812213455', 'ortopedia');
+insert into ausiliarioAppuntamento values ('c0112358', '2019-01-01','2019-01-01','e011235812213455', 'urologia');
+
