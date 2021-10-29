72034e9138c1a53a0d2fb7b8ac01dbe2419f3e89
Firamon <calabrigo.massimo@virgilio.it>
2021-10-29 12:12:07 +0200

    Relazione: Stesura ER

--- massimo_R_diagrams.r
+++ massimo_R_diagrams.r
@@ -9,14 +9,15 @@ con <- dbConnect(drv,
 	password="postgres")
 # Settiamo lo schema così non serve indicarlo
 dbGetQuery(con, "set search_path to studio")
+
+pdf("pazienti per medico.pdf")
 df <- dbGetQuery(con,"SELECT codicemedico, count(distinct cf) FROM medicoappuntamento GROUP BY codicemedico ORDER BY codicemedico")
 matr <- matrix(df$count, nrow=100)
 rownames(matr) <- df$codicemedico
 matrT <- t(matr)
 barplot(matrT, main="numero di pazienti per ogni medico", xlab="codice medico", ylab="numero di pazienti", las=2)
-pdf("pazienti per medico.pdf")
 
-
+pdf("ore per categoria.pdf")
 df_med_ext <- dbGetQuery(con,"select medicoregistra.codiceMedico, tipo, (SUM(ordinario) + SUM(straordinario)) as \"sum\" from medicoregistra join medico on medico.codicemedico = medicoregistra.codicemedico group by medicoregistra.codiceMedico, tipo having tipo <> 'interno'")
 
 df_med_int <- dbGetQuery(con,"select medicoregistra.codiceMedico, tipo, (SUM(ordinario) + SUM(straordinario)) as \"sum\" from medicoregistra join medico on medico.codicemedico = medicoregistra.codicemedico group by medicoregistra.codiceMedico, tipo having tipo <> 'esterno'")
@@ -26,4 +27,5 @@ df_aus_ass <- dbGetQuery(con,"select ausiliarioregistra.codicePersonale, tipo, (
 df_aus_amm <- dbGetQuery(con,"select ausiliarioRegistra.codicePersonale, tipo, (SUM(ordinario) + SUM(straordinario)) as \"sum\" from ausiliarioregistra join membroPersonaleAusiliario on membroPersonaleAusiliario.codicePersonale = ausiliarioregistra.codicePersonale group by ausiliarioregistra.codicePersonale, tipo having tipo = 'amministrativo'")
 
 boxplot(df_med_ext$sum, df_med_int$sum, df_aus_ass$sum, df_aus_amm$sum, names=c("medici esterni", "medici interni", "ausiliari ass med", "ausiliari amm"))
-pdf("ore per categoria.pdf")
+
+dev.off()
