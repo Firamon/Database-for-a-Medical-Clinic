503a9e0233c61d7aecf4867ce9e85b78d271599a
Firamon <calabrigo.massimo@gmail.com>
2021-11-28 11:45:49 +0100

    modified andamento ore

--- massimo_R_diagrams.r
+++ massimo_R_diagrams.r
@@ -10,14 +10,14 @@ con <- dbConnect(drv,
 # Settiamo lo schema così non serve indicarlo
 dbGetQuery(con, "set search_path to studio")
 
-pdf("pazienti per medico.pdf")
+pdf("pazienti_per_medico.pdf")
 df <- dbGetQuery(con,"SELECT codicemedico, count(distinct cf) FROM medicoappuntamento GROUP BY codicemedico ORDER BY codicemedico")
 matr <- matrix(df$count, nrow=100)
 rownames(matr) <- df$codicemedico
 matrT <- t(matr)
 barplot(matrT, main="numero di pazienti per ogni medico", xlab="codice medico", ylab="numero di pazienti", las=2)
 
-pdf("ore per categoria.pdf")
+pdf("ore_per_categoria.pdf")
 df_med_ext <- dbGetQuery(con,"select medicoregistra.codiceMedico, tipo, (SUM(ordinario) + SUM(straordinario)) as \"sum\" from medicoregistra join medico on medico.codicemedico = medicoregistra.codicemedico group by medicoregistra.codiceMedico, tipo having tipo <> 'interno'")
 
 df_med_int <- dbGetQuery(con,"select medicoregistra.codiceMedico, tipo, (SUM(ordinario) + SUM(straordinario)) as \"sum\" from medicoregistra join medico on medico.codicemedico = medicoregistra.codicemedico group by medicoregistra.codiceMedico, tipo having tipo <> 'esterno'")
@@ -26,6 +26,6 @@ df_aus_ass <- dbGetQuery(con,"select ausiliarioregistra.codicePersonale, tipo, (
 
 df_aus_amm <- dbGetQuery(con,"select ausiliarioRegistra.codicePersonale, tipo, (SUM(ordinario) + SUM(straordinario)) as \"sum\" from ausiliarioregistra join membroPersonaleAusiliario on membroPersonaleAusiliario.codicePersonale = ausiliarioregistra.codicePersonale group by ausiliarioregistra.codicePersonale, tipo having tipo = 'amministrativo'")
 
-boxplot(df_med_ext$sum, df_med_int$sum, df_aus_ass$sum, df_aus_amm$sum, names=c("medici esterni", "medici interni", "ausiliari ass med", "ausiliari amm"))
+boxplot(df_med_ext$sum, df_med_int$sum, df_aus_ass$sum, df_aus_amm$sum, names=c("medici esterni", "medici interni", "ausiliari ass med", "ausiliari amm"), main="ore cumulative per categoria", xlab="categoria", ylab="ore lavorate")
 
 dev.off()
